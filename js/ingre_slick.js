c85ac26ac06c3d799a8a89e57fead4803a07189f
hi100a <112834885+hi100a@users.noreply.github.com>
2022-09-27 15:22:46 +0900

    20220927_04

--- js/ingre_slick.js
+++ js/ingre_slick.js
@@ -5,14 +5,21 @@ $(function(){
     asNavFor: '.slider-for',
     dots: true,
     // centerMode: true,
-    focusOnSelect: true
+    focusOnSelect: true,
+    autoplay : true,
+    autoplaySpeed : 2000,
+    pauseOnHover : true,
   });
     $('.slider-for').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
         fade: true,
+        autoplay : true,
+        autoplaySpeed : 2000,
+        pauseOnHover : true,
         asNavFor: '.slider-nav'
+        
       });
 
 })
